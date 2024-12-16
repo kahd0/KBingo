414cf6240d5cda2b6c89eadfc547e1e028b8f757
kahd0 <v.rikardo@gmail.com>
2024-12-16 10:16:34 -0300

    reiniciando

--- src/utils/appState.py
+++
@@ -1,6 +0,0 @@
-class AppState:
-    def __init__(self):
-        self.currentUser = None
-        self.currentRole = None
-
-state = AppState()
--- src/variables.py
+++
@@ -1,6 +0,0 @@
-import os
-
-# Caminho correto do banco de dados
-BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
-DB_PATH = os.path.join(BASE_DIR, "data", "app.db")
-LOGO_PATH = os.path.join(BASE_DIR, "assets", "logo.png")
